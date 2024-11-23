043dbf2e7e25d31b3441230432a7fa40c5acd022
Robert Brown <robert.brown@ext.ec.europa.eu>
2024-11-23 11:21:11 +0100

    dude for realll!

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -5,13 +5,13 @@
         <div class="max-w-md mx-auto">
           <div class="divide-y divide-gray-200">
             <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-              <h2 class="text-3xl font-bold text-center mb-8 text-blue-600">Task Management System</h2>
+              <h2 class="text-3xl font-bold text-center mb-8 text-blue-600">Manage the Shizzle Bizzle</h2>
               <div class="space-y-4">
                 <router-link 
                   to="/todos" 
                   class="block w-full text-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
-                  Todo Manager
+                  Todos
                 </router-link>
                 <router-link 
                   to="/people" 
