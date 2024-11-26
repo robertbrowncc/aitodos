4a9553ec025d781786ebfdceb211b5bbf64106b2
Robert Brown <robert.brown@ext.ec.europa.eu>
2024-11-26 13:34:22 +0100

    Update: Add caching to weather API calls (1 hour cache)

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
@@ -11,17 +12,25 @@ class WeatherController extends Controller
     {
         $apiKey = config('services.openweather.key');
         $location = config('services.openweather.location');
+        $cacheKey = "weather_{$location}";
         
-        try {
-            $response = Http::get('https://api.openweathermap.org/data/2.5/weather', [
-                'q' => $location,
-                'units' => 'metric',
-                'appid' => $apiKey
-            ]);
-            
-            return $response->json();
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Failed to fetch weather data'], 500);
-        }
+        // Try to get weather data from cache
+        return Cache::remember($cacheKey, 3600, function () use ($apiKey, $location) {
+            try {
+                $response = Http::get('https://api.openweathermap.org/data/2.5/weather', [
+                    'q' => $location,
+                    'units' => 'metric',
+                    'appid' => $apiKey
+                ]);
+                
+                if ($response->successful()) {
+                    return $response->json();
+                }
+                
+                return response()->json(['error' => 'Failed to fetch weather data'], 500);
+            } catch (\Exception $e) {
+                return response()->json(['error' => 'Failed to fetch weather data'], 500);
+            }
+        });
     }
 }
